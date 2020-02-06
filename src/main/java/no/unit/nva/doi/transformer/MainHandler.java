6a2c255f6ee5977998756448ce2ad01375dd3fa5
Jan Bjørndalen <jan.bjorndalen@unit.com>
2020-02-06 10:03:08 +0100

    Get user from requestContext

--- src/main/java/no/unit/nva/doi/transformer/MainHandler.java
+++ src/main/java/no/unit/nva/doi/transformer/MainHandler.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -32,6 +33,12 @@ public class MainHandler implements RequestStreamHandler {
 
     public static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
     public static final String BODY = "body";
+    public static final String REQUEST_CONTEXT_AUTHORIZER_CLAIMS_CUSTOM_FEIDE_ID =
+            "/requestContext/authorizer/claims/custom:feideId";
+    public static final String MISSING_CUSTOM_FEIDE_ID_CLAIM_IN_REQUEST_CONTEXT =
+            "Missing custom:feideId claim in requestContext";
+    public static final String ALLOWED_ORIGIN = "ALLOWED_ORIGIN";
+    public static final String ENVIRONMENT_VARIABLE_NOT_SET = "Environment variable not set: ";
 
     private final transient ObjectMapper objectMapper;
     private final transient DataciteResponseConverter converter;
@@ -44,26 +51,29 @@ public class MainHandler implements RequestStreamHandler {
     /**
      * Constructor for MainHandler.
      *
-     * @param objectMapper  objectMapper
-     * @param converter converter
-     * @param environment   environment
+     * @param objectMapper objectMapper
+     * @param converter    converter
+     * @param environment  environment
      */
     public MainHandler(ObjectMapper objectMapper, DataciteResponseConverter converter, Environment environment) {
         this.objectMapper = objectMapper;
         this.converter = converter;
-        this.allowedOrigin = environment.get("ALLOWED_ORIGIN").orElseThrow(IllegalStateException::new);
+        this.allowedOrigin = environment.get(ALLOWED_ORIGIN)
+                .orElseThrow(() -> new IllegalStateException(ENVIRONMENT_VARIABLE_NOT_SET + ALLOWED_ORIGIN));
     }
 
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
         DataciteResponse dataciteResponse;
+        String owner;
         try {
             JsonNode event = objectMapper.readTree(input);
-            System.out.println(objectMapper.writeValueAsString(event));
             String body = event.get(BODY).textValue();
             dataciteResponse = objectMapper.readValue(body, DataciteResponse.class);
+            owner = Optional.ofNullable(event.at(REQUEST_CONTEXT_AUTHORIZER_CLAIMS_CUSTOM_FEIDE_ID).textValue())
+                    .orElseThrow(() -> new IllegalArgumentException(MISSING_CUSTOM_FEIDE_ID_CLAIM_IN_REQUEST_CONTEXT));
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
             objectMapper.writeValue(output, new GatewayResponse<>(objectMapper.writeValueAsString(
                     Problem.valueOf(BAD_REQUEST, e.getMessage())), headers(), SC_BAD_REQUEST));
             return;
@@ -71,20 +81,19 @@ public class MainHandler implements RequestStreamHandler {
 
         try {
             UUID uuid = UUID.randomUUID();
-            log("identityId: " + context.getIdentity().getIdentityId());
-            log("identityPoolId: " + context.getIdentity().getIdentityPoolId());
-            Publication publication = converter.toPublication(dataciteResponse, uuid, "owner");
+            Publication publication = converter.toPublication(dataciteResponse, uuid, owner);
+            log(objectMapper.writeValueAsString(publication));
             objectMapper.writeValue(output, new GatewayResponse<>(
                     objectMapper.writeValueAsString(publication), headers(), SC_OK));
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
             objectMapper.writeValue(output, new GatewayResponse<>(objectMapper.writeValueAsString(
                     Problem.valueOf(INTERNAL_SERVER_ERROR, e.getMessage())), headers(), SC_INTERNAL_SERVER_ERROR));
         }
     }
 
-    private Map<String,String> headers() {
-        Map<String,String> headers = new ConcurrentHashMap<>();
+    private Map<String, String> headers() {
+        Map<String, String> headers = new ConcurrentHashMap<>();
         headers.put(ACCESS_CONTROL_ALLOW_ORIGIN, allowedOrigin);
         headers.put(CONTENT_TYPE, APPLICATION_JSON.getMimeType());
         return headers;
@@ -93,16 +102,16 @@ public class MainHandler implements RequestStreamHandler {
     /**
      * Create ObjectMapper.
      *
-     * @return  objectMapper
+     * @return objectMapper
      */
     public static ObjectMapper createObjectMapper() {
         return new ObjectMapper()
                 .registerModule(new ProblemModule())
                 .registerModule(new JavaTimeModule())
                 .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-                .configure(SerializationFeature.INDENT_OUTPUT, true)
                 .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
     }
 
     public static void log(String message) {
