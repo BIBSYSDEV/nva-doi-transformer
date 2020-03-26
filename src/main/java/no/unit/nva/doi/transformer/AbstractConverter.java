8e6d52f0c6274daad1e207f763d5b2ec26bcec54
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2020-03-26 14:31:22 +0100

    NP-581: made a constant

--- src/main/java/no/unit/nva/doi/transformer/AbstractConverter.java
+++ src/main/java/no/unit/nva/doi/transformer/AbstractConverter.java
@@ -9,9 +9,10 @@ import no.unit.nva.model.PublicationStatus;
 public class AbstractConverter {
 
     public static final PublicationStatus DEFAULT_NEW_PUBLICATION_STATUS = PublicationStatus.NEW;
+    public static final String FAMILY_NAME_GIVEN_NAME_SEPARATOR = ", ";
 
     protected String toName(String familyName, String givenName) {
-        return String.join(", ", familyName, givenName);
+        return String.join(FAMILY_NAME_GIVEN_NAME_SEPARATOR, familyName, givenName);
     }
 
     protected PublicationDate toDate(Integer publicationYear) {
