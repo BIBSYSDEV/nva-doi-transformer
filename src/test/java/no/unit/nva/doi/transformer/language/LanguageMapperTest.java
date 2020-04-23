1fe4ab830c46d0bde535cb81718684a7bd63c4e1
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2020-04-23 12:57:27 +0200

    fix: constants

--- src/test/java/no/unit/nva/doi/transformer/language/LanguageMapperTest.java
+++ src/test/java/no/unit/nva/doi/transformer/language/LanguageMapperTest.java
@@ -11,17 +11,19 @@ import org.junit.jupiter.api.Test;
 class LanguageMapperTest {
 
     private static final URI expected = URI.create("http://lexvo.org/id/iso639-3/eng");
+    public static final String TWO_LETTER_ISO = "en";
+    public static final String ISO_639_3 = "eng";
 
     @Test
     public void getUriFromIso639ReturnsUriForTwoLetterLanguageCodes() {
-        Optional<URI> uri = LanguageMapper.getUriFromIsoAsOptional("en");
+        Optional<URI> uri = LanguageMapper.getUriFromIsoAsOptional(TWO_LETTER_ISO);
         assertThat(uri.isPresent(), is(equalTo(true)));
         assertThat(uri.get(), is(equalTo(expected)));
     }
 
     @Test
     public void getUriFromIso639ReturnsUriForIso639LanguageCodes() {
-        Optional<URI> uri = LanguageMapper.getUriFromIsoAsOptional("eng");
+        Optional<URI> uri = LanguageMapper.getUriFromIsoAsOptional(ISO_639_3);
         assertThat(uri.isPresent(), is(equalTo(true)));
         assertThat(uri.get(), is(equalTo(expected)));
     }
